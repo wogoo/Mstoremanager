84a769c41022572fc432d633872f41b227a4ca04
woggo <paulotimbu653@gmail.com>
2022-05-04 14:55:24 -0300

    swagger & profiles

--- src/main/java/com/wogoo/moviesstoremanager/MoviesStoreManagerApplication.java
+++ src/main/java/com/wogoo/moviesstoremanager/MoviesStoreManagerApplication.java
@@ -1,10 +1,17 @@
 package com.wogoo.moviesstoremanager;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = MoviesStoreManagerApplication.MOVIES_API, version = MoviesStoreManagerApplication.VERSION_API, description = MoviesStoreManagerApplication.API_DESC_DOC))
 public class MoviesStoreManagerApplication {
+
+	public static final String MOVIES_API = "MSM API";
+	public static final String VERSION_API = "0.0.1";
+	public static final String API_DESC_DOC = "MSM Information";
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesStoreManagerApplication.class, args);
--- src/main/java/com/wogoo/moviesstoremanager/controller/MovieController.java
+++ src/main/java/com/wogoo/moviesstoremanager/controller/MovieController.java
@@ -1,5 +1,6 @@
 package com.wogoo.moviesstoremanager.controller;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+
+
     @GetMapping("/")
     public String getHello() {
         return "Hello World!";
